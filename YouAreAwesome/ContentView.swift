4fd6899541ddeba3141edd12e6c23e0eaf45fa23
Isaiah007864 <applecoder07@gmail.com>
2025-02-12 11:37:48 -0800

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  YouAreAwsome 
+//
+//  Created by Student1 on 1/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    var body: some View {
+        
+        VStack {
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            
+            Spacer()
+            
+            Button("Press Me!") {
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great!"
+   
+                message = ( message == message1 ? message2 : message1)
+                imageName = ( imageName == "image0" ? "image1" : "image0" )
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
